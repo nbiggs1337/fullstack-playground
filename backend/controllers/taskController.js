fc07d5d1b388818f20cb1dbe88857eaf0d9efd63
MarressDev <pmarress1.prsvr@gmail.com>
2025-06-26 19:55:59 -0500

    created task controller

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,13 @@
+const createTask = async (req, res) => {
+  try {
+    const task = new Task({
+      title: req.body.title,
+      description: req.body.description,
+      status: req.body.status,
+    });
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
